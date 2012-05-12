7d8e630b5a3dd8c133d596dbeea5bcbb0305e811
= <=>
2012-05-12 16:25:38 +0800

    none

--- ch1/rearrang.c
+++ ch1/rearrang.c
@@ -5,8 +5,7 @@
 #define MAX_INPUT 1000	/* Max len of input & output lines */
 
 int read_column_numbers(int columns[], int max);
-void rearrange(char *output, char const *input,
-	int n_columns, int const columns[]);
+void rearrange(char *output, char const *input, int n_columns, int const columns[]);
 
 int main(void)
 {
